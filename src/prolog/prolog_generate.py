0e1f9128d6388f35606ff96c516eb3106bada89b
sfhelmet <sfhelmet@gmail.com>
2023-12-05 21:46:08 -0500

    fix bug creating wrong states

--- src/prolog/prolog_generate.py
+++ src/prolog/prolog_generate.py
@@ -6,6 +6,7 @@ sys.path.append(root_path)
 
 from model.state import State
 from model.transition import Transition
+from model.action import Action
 from prolog.prolog_constants import NIL
 
 def generate_prolog(states: dict[str:State], transitions: set[Transition]) -> str:
@@ -16,23 +17,33 @@ def generate_prolog(states: dict[str:State], transitions: set[Transition]) -> st
     choice = []
     entry_pseudostates = []
     exit_pseudostates = []
+    on_entry_actions_list = []
+    do_actions_list = []
+    on_exit_actions_list = []
     for state_name in states:
         state = states[state_name]
+        if state.superstate or state.entry or state.exit:
+            if state.superstate:
+                super_states.append(state.name)
+            if state.entry:
+                entry_pseudostates.append(state.name)
+            if state.exit:
+                exit_pseudostates.append(state.name)
+        else:
+            prolog_code += "state(" + state.name + ").\n"
+
         if state.is_initial:
             initial_states.append(state.name)
         if state.is_final:
             final_states.append(state.name)
-        if state.superstate:
-            super_states.append(state.name)
         if state.choice:
             choice.append(state.name)
-        if state.entry:
-            entry_pseudostates.append(state.name)
-        if state.exit:
-            exit_pseudostates.append(state.name)
-        else:
-            prolog_code += "state(" + state.name + ").\n"
-
+        if state.on_entry_actions:
+            on_entry_actions_list.append(state.name)
+        if state.do_actions:
+            do_actions_list.append(state.name)
+        if state.on_exit_actions:
+            on_exit_actions_list.append(state.name)
             
     for initial_state in initial_states:
         prolog_code += "initial(" + initial_state + ").\n"
@@ -57,6 +68,10 @@ def generate_prolog(states: dict[str:State], transitions: set[Transition]) -> st
         
     for exit_pseudostate in exit_pseudostates:
         prolog_code += f"exit_pseudostate({exit_pseudostate}, {states[exit_pseudostate].superstate}).\n"
+
+    for on_entry_action_state in on_entry_actions_list:
+        for action in states[on_entry_action_state].on_entry_actions:
+            prolog_code += f"onentry_action({on_entry_action_state}, {create_action_string(action)}).\n"
 
     prolog_code += "\n"
 
@@ -92,3 +107,6 @@ def generate_prolog(states: dict[str:State], transitions: set[Transition]) -> st
         prolog_code += f"transition({src}, {dest}, {event_str}, {guard_str}, {action_str}).\n" 
 
     return prolog_code
+
+def create_action_string(action: Action) -> str:
+    return f'action({action.type}, "{action.parameter}")'
