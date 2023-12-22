712f1542cb8005541a8ced408461c5a50b5dfd1d
sfhelmet <sfhelmet@gmail.com>
2023-12-22 06:04:08 -0500

    refactor prolog generation constants

--- src/prolog/prolog_generate.py
+++ src/prolog/prolog_generate.py
@@ -7,7 +7,7 @@ sys.path.append(root_path)
 from model.state import State
 from model.transition import Transition
 from model.action import Action
-from prolog.prolog_constants import NIL
+from prolog.prolog_constants import *
 
 def generate_prolog(states: dict[str:State], transitions: set[Transition]) -> str:
     prolog_code = ""
@@ -30,7 +30,7 @@ def generate_prolog(states: dict[str:State], transitions: set[Transition]) -> st
             if state.exit:
                 exit_pseudostates.append(state.name)
         else:
-            prolog_code += "state(" + state.name + ").\n"
+            prolog_code += f"{STATE_PREFIX}({state.name}).\n"
 
         if state.is_initial:
             initial_states.append(state.name)
@@ -46,40 +46,40 @@ def generate_prolog(states: dict[str:State], transitions: set[Transition]) -> st
             on_exit_actions_list.append(state.name)
             
     for initial_state in initial_states:
-        prolog_code += "initial(" + initial_state + ").\n"
+        prolog_code += f"{INITIAL_PREFIX}({initial_state}).\n"
     
     for final_state in final_states:
-        prolog_code += "final(" + final_state + ").\n"
+        prolog_code += f"{FINAL_PREFIX}({final_state}).\n"
     
     for alias in final_states:
-        prolog_code += "alias(" + alias + ", '').\n"
+        prolog_code += f"{ALIAS_PREFIX}({alias}, '').\n"
 
     for state in super_states:
-        prolog_code += "superstate(" + states[state].superstate + ", " + state + ").\n"
+        prolog_code += f"{SUPERSTATE_PREFIX}({states[state].superstate}, {state}).\n"
 
     for state in choice:
-        prolog_code += f"state({state}). % choice state\n"
+        prolog_code += f"{CHOICE_STATE_PREFIX}({state}). % choice state\n"
 
     for entry_pseudostate in entry_pseudostates:
         for transition in transitions:
             if transition.source == entry_pseudostate:
-                prolog_code += f"entry_pseudostate({entry_pseudostate}, {transition.destination}).\n"
+                prolog_code += f"{ENTRY_PSEUDOSTATE_PREFIX}({entry_pseudostate}, {transition.destination}).\n"
                 break
         
     for exit_pseudostate in exit_pseudostates:
-        prolog_code += f"exit_pseudostate({exit_pseudostate}, {states[exit_pseudostate].superstate}).\n"
+        prolog_code += f"{EXIT_PSEUDOSTATE_PREFIX}({exit_pseudostate}, {states[exit_pseudostate].superstate}).\n"
 
     for on_entry_action_state in on_entry_actions_list:
         for action in states[on_entry_action_state].on_entry_actions:
-            prolog_code += f"onentry_action({on_entry_action_state}, {create_action_string(action)}).\n"
+            prolog_code += f"{ON_ENTRY_ACTION_PREFIX}({on_entry_action_state}, {create_action_string(action)}).\n"
 
     for do_action_state in do_actions_list:
         for proc in states[do_action_state].do_actions:
-            prolog_code += f"do_action({do_action_state}, {create_proc_string(proc)}).\n"
+            prolog_code += f"{DO_ACTION_PREFIX}({do_action_state}, {create_proc_string(proc)}).\n"
 
     for on_exit_action_state in on_exit_actions_list:
         for action in states[on_exit_action_state].on_exit_actions:
-            prolog_code += f"onexit_action({on_exit_action_state}, {create_action_string(action)}).\n"
+            prolog_code += f"{ON_EXIT_ACTION_PREFIX}({on_exit_action_state}, {create_action_string(action)}).\n"
 
     prolog_code += "\n"
 
@@ -89,7 +89,7 @@ def generate_prolog(states: dict[str:State], transitions: set[Transition]) -> st
 
         event_type = transition.events[0].type if len(transition.events) != 0 else NIL
         event_parameter = transition.events[0].parameter if len(transition.events) != 0 else NIL
-        event_str = f'event({event_type}, "{event_parameter}")' if len(transition.events) != 0 else NIL
+        event_str = f'{EVENT_PREFIX}({event_type}, "{event_parameter}")' if len(transition.events) != 0 else NIL
 
         if len(transition.guards) == 0:
             guard_str = NIL
@@ -110,14 +110,14 @@ def generate_prolog(states: dict[str:State], transitions: set[Transition]) -> st
                 actions_parameter_str += "; "
             actions_parameter_str += f'{action.parameter}'
 
-        action_str = f'action({transition.actions[0].type}, "{actions_parameter_str}")' if len(transition.actions) != 0 else NIL
+        action_str = f'{ACTION_PREFIX}({transition.actions[0].type}, "{actions_parameter_str}")' if len(transition.actions) != 0 else NIL
 
-        prolog_code += f"transition({src}, {dest}, {event_str}, {guard_str}, {action_str}).\n" 
+        prolog_code += f"{TRANSITION_PREFIX}({src}, {dest}, {event_str}, {guard_str}, {action_str}).\n" 
 
     return prolog_code
 
 def create_action_string(action: Action) -> str:
-    return f'action({action.type}, "{action.parameter}")'
+    return f'{ACTION_PREFIX}({action.type}, "{action.parameter}")'
 
 def create_proc_string(proc: str) ->str:
-    return f'proc("{proc}")'
+    return f'{PROCEDURE_PREFIX}("{proc}")'
