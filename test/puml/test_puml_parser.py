734812299ec57167eef4703aa493ffa3388173af
sfhelmet <sfhelmet@gmail.com>
2024-05-07 22:03:32 -0400

    fix test

--- test/puml/test_puml_parser.py
+++ test/puml/test_puml_parser.py
@@ -1,10 +1,10 @@
 import pytest
-from src.puml.puml_parser import parse_ega
+from src.puml.puml_parser import __parse_ega
 
 @pytest.mark.unit
 def test_parse_ega():
     ega = "event1 [guard1] / action1"
-    events, guards, actions = parse_ega(ega)
+    events, guards, actions = __parse_ega(ega)
     assert events[0].parameter == "event1"
     assert guards[0].condition == "guard1"
     assert actions[0].parameter == "action1"
@@ -12,7 +12,7 @@ def test_parse_ega():
 @pytest.mark.unit
 def test_parse_ega_list():
     ega = "event1; event2 [guard1; guard2] / action1; action2"
-    events, guards, actions = parse_ega(ega)
+    events, guards, actions = __parse_ega(ega)
     assert str(events[0]) == "event1"
     assert str(events[1]) == "event2"
     assert str(guards[0]) == "guard1"
